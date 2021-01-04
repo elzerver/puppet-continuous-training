4c7e8ab0770b11a7d2c407bba4d29353f58252a8
aldo <test@tester.com>
2021-01-04 13:58:11 +0100

    update from example

--- environments/production/modules/nginx/manifests/vhosts.pp
+++ environments/production/modules/nginx/manifests/vhosts.pp
@@ -1,18 +1,18 @@
-#  Generate a virtual hosts file for nginx
-#  @summary Generate a virtual hotss file for nginx
-# 
+# Generate a virtual hosts file for nginx. Requires node-specific Hiera data
+#
+# @summary Generate a virtual hosts file for nginx
 #
 # @example
 #   include nginx::vhosts
 class nginx::vhosts (
-  $vhost_dir = $nginx::params::vhosts_dir,
+  $vhosts_dir = $nginx::params::vhosts_dir,
 ) inherits nginx::params {
-  file { "${nginx::vhost_name}.conf":
-     content => epp('nginx/vhosts.conf.epp'),
-     ensure => $nginx::vhost_ensure,
-     path => "${vhosts_dir}/${nginx::vhost_name}.conf",
+  file { "${nginx::vhosts_name}.conf":
+    content => epp('nginx/vhosts.conf.epp'),
+    ensure  => $nginx::vhosts_ensure,
+    path    => "${vhosts_dir}/${nginx::vhosts_name}.conf",
   }
 
   file { "$nginx::vhosts_root":
-     ensure => 'directory',
-}
+    ensure => 'directory',
+  }
