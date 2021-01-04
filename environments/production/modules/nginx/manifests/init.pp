77fcb492b77e273954d8a69ea7cd1dd44c49afc3
aldo <test@tester.com>
2021-01-04 13:42:11 +0100

    add the node specific configuration

--- environments/production/modules/nginx/manifests/init.pp
+++ environments/production/modules/nginx/manifests/init.pp
@@ -9,17 +9,24 @@ class nginx (
    $config_path = $nginx::params::config_path,
    $config_source = $nginx::params::config_source,
    $service_name = $nginx::params::service_name,
+   $vhost_dir = $nginx::params::vhost_dir,
    String $package_ensure,
    String $config_ensure,
    String $service_ensure,
    Boolean $service_enable,
    Boolean $service_restart,
+   String $vhost_port,
+   String $vhost_root,
+   String $vhost_ensure,
+   String $vhost_name, 
 ) inherits nginx::params {
    contain nginx::install
    contain nginx::config
    contain nginx::service
+   contain::nginx::vhosts
 
    Class['nginx::install']
    -> Class['nginx::config']
    ~> Class['nginx::service']
+   -> Class['nginx::vhosts']
 }
--- environments/production/modules/nginx/manifests/vhosts.pp
+++ environments/production/modules/nginx/manifests/vhosts.pp
@@ -1,8 +1,17 @@
-# @summary A short summary of the purpose of this class
-#
-# A description of what this class does
+#  Generate a virtual hosts file for nginx
+#  @summary Generate a virtual hotss file for nginx
+# 
 #
 # @example
 #   include nginx::vhosts
-class nginx::vhosts {
+class nginx::vhosts (
+  $vhost_dir = $nginx::params::vhosts_dir,
+) inherits nginx::params {
+  file { "${nginx::vhost_name}.conf":
+     content => epp('nginx/vhosts.conf.epp'),
+     ensure => $nginx::vhost_ensure,
+     path => "${vhost_dir}/${nginx::vhost_name}.conf"
+  }
+  file "$nginx::vhosts_root":
+     ensure => 'directory',
 }
